7cd7aaaed7718a1c7601b41c805541301af9e4bf
André <108902451+DreZitus@users.noreply.github.com>
2025-11-05 23:45:43 +0000

    adicionario dicionario e utilização de função dict

--- script.py
+++ script.py
@@ -10,13 +10,17 @@ else:
     print('Resposta falhou')
 
 ##print(res.headers)
-##int(res.text)
+int(res.text)
 
 
-
+#pegando chave da API
 API_KEY = 'your yandex api key' 
 
 url = 'https://translate.yandex.net/api.v1.5/tr.json/translate'
-res = requests.get.(url)
 
-#rode usando python scripy.py no terminal
+#criação de dicionario com a função dict
+params = dict(key=API_KEY, text='Hello', lang='en-es')
+
+res = requests.get(url, params=params)
+
+print(res.text)
