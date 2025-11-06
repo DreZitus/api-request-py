279cabb4a7edaf9d7584d473717d85099b202785
André <108902451+DreZitus@users.noreply.github.com>
2025-11-06 00:25:50 +0000

    a

---
+++ request-py/script.py
@@ -0,0 +1,26 @@
+import requests
+
+res = requests.get('https://scotch.io')
+
+print(res)
+
+if res:
+    print('resposta OK')
+else:
+    print('Resposta falhou')
+
+#print(res.headers)
+#int(res.text)
+
+
+#pegando chave da API
+API_KEY = 'your yandex api key' 
+
+url = 'https://translate.yandex.net/api.v1.5/tr.json/translate'
+
+#criação de dicionario com a função dict
+params = dict(key=API_KEY, text='Hello', lang='en-es')
+
+res = requests.get(url, params=params)
+
+print(res.text)
